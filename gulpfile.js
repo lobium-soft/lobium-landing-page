9b76b5ff505d2da1f9ffc30523fe8dcbfd14355f
Jonathan Roy <jonathanroy@MacBookPro.hitronhub.home>
2025-04-08 11:17:32 -0400

    Update express configuration + Add config to use basic auth

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
+
 function startExpressWithHotReload() {
 	const browserSync = require( 'browser-sync' ),
 		gulp = require( 'gulp' );
 
-	const app = expressSetup();
+	const app = expressSetup({ 
+		useBasicAuth: false,
+		debug: true 
+	});
 
 	// listens to server at PORT
 	app.listen( 3000, function () {
@@ -41,12 +45,34 @@ function startExpress() {
 	});
 }
 
-function expressSetup () {
+function startExpressWithBasicAuth() {
+	const app = expressSetup({
+		useBasicAuth: true,
+		debug: false
+	});
+
+	// listens to server at PORT
+	app.listen( 3000, function () {
+		console.log( `[Express] Server with basic auth listening on port 3000` );
+	});
+}
+
+function expressSetup (config = { useBasicAuth: false, debug: false }) {
 	const
 		express        = require( 'express' ),
+		expressBasicAuth = require( 'express-basic-auth' ),
 		sassMiddleware = require( 'node-sass-middleware' ),
 		path           = require( 'path' ),
 		app            = express();
+
+	if (config.useBasicAuth) {
+		app.use( expressBasicAuth({
+			users: {
+				lobium: 'jomathlobium'
+			},
+			challenge: true
+		}));
+	}
 
 	// sets template engine
 	app.set( 'view engine', 'pug' );
@@ -55,25 +81,22 @@ function expressSetup () {
 	app.set( 'views', './src/' );
 
 	app.get( '/', function ( req, res ) {
-		console.log( '[request] root: index.pug' );
 		res.render( `index.pug` );
 	});
 
 	app.get( /.+\.pug/, function ( req, res ) {
-		console.log( `[request] pug: ${req.url}` );
 		res.render( `${req.url}` );
 	});
 
 	app.get( /.+\.html/, function ( req, res ) {
 		let tmp = req.url.replace( /html$/, 'pug' );
-		console.log( '[request] html:', tmp );
 		res.render( `${tmp}` );
 	});
 
 	app.use( sassMiddleware({
 		src: path.resolve( __dirname, 'src' ),
 		dest: path.resolve( __dirname, 'src' ),
-		debug: true,
+		debug: config.debug,
 		outputStyle: 'expanded',
 		indentType: 'tab',
 		indentWidth: 1,
@@ -87,4 +110,5 @@ function expressSetup () {
 }
 
 exports['dev'] = startExpressWithHotReload;
+exports['start-with-basic-auth'] = startExpressWithBasicAuth;
 exports['start'] = startExpress;
